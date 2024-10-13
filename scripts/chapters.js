3d2d103933f57f95becd446a4ff22969689b54cb
tabishaliansari <141923176+tabishaliansari@users.noreply.github.com>
2024-10-13 13:01:02 +0530

    Added hover menu items

--- scripts/chapters.js
+++ scripts/chapters.js
@@ -1,41 +1,56 @@
+//
+//	Author: munawwar_ali@yahoo.com
+//
 
 function getLocationPath(){
 	return window.location.href.substring(0,window.location.href.lastIndexOf("/")+1);
 }
 
-function openChapter(file){
-	
+function openChapter(file, page){
+	console.log("file: " + file);
 	var loaction = window.location.href.substring(0,window.location.href.lastIndexOf("/")+1);
 	var abs_path = getLocationPath() + 'pdfs/' + file;
-	console.log("opening '"+ abs_path + "'");
 	
-	$('.reading-pane').attr('src', encodeURI(abs_path));
+	if(page > 0){
+		console.log("opening '"+ abs_path + "' , page = " + page);
+		$('.reading-pane').attr("src","");
+		setTimeout(function(){
+			$('.reading-pane').attr('src', encodeURI(abs_path)+"#page=" + page);
+		}, 5);
+	}
+	else{
+		console.log("opening '"+ abs_path + "'");
+		$('.reading-pane').attr("src","");
+		setTimeout(function(){
+			$('.reading-pane').attr('src', encodeURI(abs_path));
+		}, 5);
+	}
 	$('body img').hide();
 }
 
 $(function () {
 		
 	var suffix = " - Arabic.pdf";
-	var alug_chapters = [
-		"P0.1 - Basics", 
-		"P0.2 - Vocabulary", 
-		"P0.3 - Sample Dictionary", 
-		"P1 - Pronouns", 
-		"P2 - Nouns", 
-		"P2.1 - Noun Patterns", 
-		"P2.2 - Object Effects", 
-		"P3 - Verbs", 
-		"P3.1 - Verb Conjugations", 
-		"P4 - Sentences", 
-		"P5 - Particles"
-	];
 	
-	alug_chapters.forEach(function(ch, index)
+	// Create navigation menus
+	for (const [ch, links] of Object.entries(alug_chapters)) 
 	{
 		var chapterPath = '\''+ch+suffix+'\''; 
 		console.log(chapterPath);
 		var chapterName = ch.split('-')[1].trim();
-		$('<div class="chapter-card" onclick="openChapter('+chapterPath+')" >'+chapterName+'</div>')
-			.appendTo('.card-container');
-	});
+		$div = $('<div class="chapter-card"><span onclick="openChapter('+chapterPath+', 0)" >'+chapterName+'</span></div>');
+		$('.card-container').append($div);
+			
+		if(links && links.length > 0){
+			console.log('Links found');
+			$subNav = $('<div class="subnav-content"></div>');
+			links.forEach(function(link, index){
+				
+				$subNav.append($('<div class="subNav" onclick="openChapter('+chapterPath+',' + link.pageNo+')">'+link.topic+'</div>'));				
+				console.log('Added '+ link.topic);
+			});
+			$div.append($subNav);
+		}
+	}
+		
 });
---
+++ scripts/data.js
@@ -0,0 +1,119 @@
+//
+//	Author: munawwar_ali@yahoo.com
+//
+
+var alug_chapters = {
+	"P0.1 - Basics": [
+		{topic: "Parts Of Speech", pageNo: 2}, 
+		{topic: "Alphabets", pageNo: 4},
+		{topic: "علامات التشكيل vs الاعراب", pageNo: 5},
+		{topic: "Alif vs Hamza", pageNo: 6},
+		{topic: "همزة القطع vs همزة الوصل ", pageNo: 7},
+		{topic: "التَجوِيد (Tajweed)", pageNo: 8}, 
+		{topic: "Diacritic rules", pageNo: 10},
+		{topic: "Word forms - الاوزان", pageNo: 11}
+	],
+	"P0.2 - Vocabulary": [
+		{topic: "Alphabets", pageNo: 2}, 
+		{topic: "Homonyms", pageNo: 4}, 
+		{topic: "Synonyms", pageNo: 6},
+		{topic: "Antonyms", pageNo: 9},
+		{topic: "Jar (حروف الجر)", pageNo: 11},
+		{topic: "Verbs", pageNo: 12},
+		{topic: "Verb Forms", pageNo: 14},
+		{topic: "Imperatives", pageNo: 16},
+		{topic: "Colours (ألوان)", pageNo: 18},
+		{topic: "Time Chart", pageNo: 19},
+		{topic: "Numbers", pageNo: 20},
+		{topic: "Proverbs (الأمثال)", pageNo: 25}
+	],
+	"P0.3 - Sample Dictionary": [
+	], 
+	"P1 - Pronouns": [
+		{topic: "Possesive Constructs ", pageNo: 3}, 
+		{topic: "واؤ الإشباع", pageNo: 4}, 
+		{topic: "اسماء الإشارة", pageNo: 5},
+		{topic: "الاسم الموصول", pageNo: 6},
+		{topic: "ذُو vs ذات vs أُولو", pageNo: 7}
+	],
+	"P2 - Nouns": [
+		{topic: "إعرابُ الأسماء", pageNo: 3}, 
+		{topic: "Definite (مَعرِفَة) vs Indefinite (نَكِرَة)", pageNo: 6}, 
+		{topic: "Masculine vs Feminine (تاء التّانيس)", pageNo: 7},
+		{topic: "Sound and Irregular Plurals", pageNo: 9},
+		{topic: "Adjectival Construction - النعت والمنعُوت", pageNo: 13},
+		{topic: "Substitute - البدل", pageNo: 14},
+		{topic: "Empathetic - التوكيد", pageNo: 15},
+		{topic: "Possessive Construction  - اضافة", pageNo: 16},
+		{topic: "The Five Nouns (اسماء الخمسة)", pageNo: 20}
+	], 
+	"P2.1 - Noun Patterns": [
+		{topic: "إسمُ الآلة and إِسمُ الزمان ومكان", pageNo: 2}, 
+		{topic: "Superlative (إسمُ التفضيل) & Diminutive", pageNo: 3}, 
+		{topic: "إسم المبالغة – Hyperbole", pageNo: 4},
+		{topic: "Irregular Plural Patterns", pageNo: 5},	
+		{topic: "إسمُ الفاعل وإسمُ المفعُول", pageNo: 6}
+	], 
+	"P2.2 - Object Effects": [
+		{topic: "Direct Object (المفعول به)", pageNo: 2}, 
+		{topic: "Absolute Effect (المفعول المطلق)", pageNo: 3}, 
+		{topic: "Adverb (المفعول فيه (الظرف", pageNo: 4},
+		{topic: "Adverbs of frequency (ظروف التكرار)", pageNo: 5},	
+		{topic: "Adverbs of manner (الحال)", pageNo: 6},
+		{topic: "The Causal Object المفعول لأجله / المفعول له", pageNo: 7},
+		{topic: "The Comitative Object المفعول معه", pageNo: 8},
+		{topic: "Circumstantial Adverb (الحال)", pageNo: 9},
+		{topic: "Distinction (التمييز)", pageNo: 10},
+		{topic: "Exclusion (المستثنى)", pageNo: 11},
+		{topic: "Vocative (المنادى)", pageNo: 12}
+	], 
+	"P3 - Verbs": [
+		{topic: "ثلاثي مُجرَّد – Verb Forms (I)", pageNo: 4}, 
+		{topic: "ثلاثي مزيد - Verb Forms (I to X)", pageNo: 5}, 
+		{topic: "Verb Ending - Cases", pageNo: 10}, 
+		{topic: "المعلوم - Active Voice", pageNo: 11}, 
+		{topic: "المجهُول – Passive Voice", pageNo: 12}, 
+		{topic: "Adequate  Verbs ( افعال التامّة )", pageNo: 13}, 
+		{topic: "Inadequate Verbs ( الافعال الناقصة)", pageNo: 14}, 
+		{topic: "Phrasal Verbs (أفعالُ المُركّبة )", pageNo: 15}, 
+		{topic: "Double Consonant Verbs (مضاعفة)", pageNo: 18}, 
+		{topic: "Weak Verbs - الافعال المعتلة", pageNo: 19}
+	], 
+	"P3.1 - Verb Conjugations": [
+		{topic: "Form I", pageNo: 2}, 
+		{topic: "Form I  - Defective (مثال)", pageNo: 3}, 
+		{topic: "Form I  - Defective (اجوف)", pageNo: 4}, 
+		{topic: "Form I  - Defective (ناقص)", pageNo: 5}, 
+		{topic: "Imperatives- Conjugation (ناقص)", pageNo: 6}
+		
+	], 
+	"P4 - Sentences": [
+		{topic: "Nominal Sentences", pageNo: 2}, 
+		{topic: "Verbal Sentences", pageNo: 12}, 
+		{topic: "Subject Verb Agreement", pageNo: 13}, 
+		{topic: "Future Tense (المُستقبل فعلُ)", pageNo: 14}, 
+		{topic: "Imperatives (الأمر والنهى)", pageNo: 15}, 
+		{topic: "Two-Verb Combinations", pageNo: 18}, 
+		{topic: "نُونُ الوِقايَة and  ياءُ المُتكلِّم and ياءُ المُخاطبة", pageNo: 19}, 
+		{topic: "اِنَّ  and its sisters", pageNo: 5}, 
+		{topic: "كانَ  and its sisters", pageNo: 7}, 
+		{topic: "كادَ   and its sisters", pageNo: 9}, 
+		{topic: "ظَنَّ   and its sisters", pageNo: 10}, 
+		{topic: "Miscellaneous phrases", pageNo: 21}, 
+	], 
+	"P5 - Particles": [
+		{topic: "Conjunctions", pageNo: 3}, 
+		{topic: "نواصب الفعل", pageNo: 5}, 
+		{topic: "جوازم الفعل", pageNo: 6}, 
+		{topic: "أَنْ  vs أَنَّ vs  إِنَّ", pageNo: 7}, 
+		{topic: "Uses of  ل", pageNo: 8}, 
+		{topic: "Uses of لا", pageNo: 9}, 
+		{topic: "Uses of  ما", pageNo: 10}, 
+		{topic: "Uses of  إِن", pageNo: 11}, 
+		{topic: "Uses of  و", pageNo: 12}, 
+		{topic: "Uses of  إذ /  إذا", pageNo: 14}, 
+		{topic: "Uses of قبلُ (بعد, etc.)", pageNo: 15}, 
+		{topic: "Uses of مِن قِبَلِ", pageNo: 16}, 
+		{topic: "Vocative Particles (النداء)", pageNo: 17} 
+	]
+};
