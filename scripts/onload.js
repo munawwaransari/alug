f718b045b96fa0e4f516c46add02542cf44276bd
tabishaliansari <141923176+tabishaliansari@users.noreply.github.com>
2024-10-18 22:33:27 +0530

    added speech synthesis support

---
+++ scripts/onload.js
@@ -0,0 +1,16 @@
+window.onload = function()
+{
+	$("#languages").hide();
+	var support = document.getElementById("support").innerHTML;
+
+	if(support.startsWith("Hurray")){
+		$("#s3_2").hide();
+		$("#s3_1").show();
+		$("#s3_1+p").text(support);
+	}
+	else{
+		$("#s3_1").hide();
+		$("#s3_2").show();
+		$("#s3_2+p").text(support);
+	}
+}
---
+++ scripts/test.js
@@ -0,0 +1,5 @@
+var Test = {
+	
+};
+
+exports.default = Test;
