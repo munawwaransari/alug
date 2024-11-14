8430b8c968fa3d9b91aaacdfe9218b5445939b44
munawwaransari <munawwar_ali@yahoo.com>
2024-11-14 15:29:01 +0530

    Added alphabet chart

---
+++ scripts/data-util.js
@@ -0,0 +1,52 @@
+function getLocationPath(){
+	return window.location.href.substring(0,window.location.href.lastIndexOf("/")+1);
+
+}
+
+function getParamValue(paramName){
+	var url = window.location.search.substring(1); //get rid of "?" in querystring
+	var qArray = url.split('&'); //get key-value pairs
+	for (var i = 0; i < qArray.length; i++) 
+	{
+		var pArr = qArray[i].split('='); //split key and value
+		if (pArr[0] == paramName) 
+			return pArr[1]; //return value
+	}
+}
+
+async function loadJsonData(url,  callback, errorCallback)
+{
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+		const data = await response.json();
+
+		callback(data);
+	} 
+	catch (error) {
+
+		console.error("Fetch error:", error);
+		if (errorCallback){
+			errorCallback(error);
+		}
+	}
+}
+
+async function loadHtmlData(url,  callback)
+{
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+		const data = await response.text();
+		callback(data);
+	} 
+	catch (error) {
+		console.error("Fetch error:", error);
+	}
+}
