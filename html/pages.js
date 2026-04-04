610c0d242362576545502facf85e05beac1273d0
munawwaransari <munawwar_ali@yahoo.com>
2026-04-04 11:38:50 +0530

    refactored

--- html/pages.js
+++ html/pages.js
@@ -1,17 +1,11 @@
 var pages = "123456789abcdefghijklmnopqrstuvwxyz";
  
- function getParamValue(paramName){
-	var url = window.location.search.substring(1); //get rid of "?" in querystring
-	var qArray = url.split('&'); //get key-value pairs
-	for (var i = 0; i < qArray.length; i++) 
-	{
-		var pArr = qArray[i].split('='); //split key and value
-		if (pArr[0] == paramName) 
-			return pArr[1]; //return value
-	}
- }
+function getParamValue(paramName) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(paramName);
+}
 
- window.onload = function(){
+window.onload = function(){
     var p = getParamValue("page");
 	if(p !== null && p !== undefined){
 		var page = (p > -1 && p < pages.length) ? pages[p-1] : -1;
--- scripts/data-util.js
+++ scripts/data-util.js
@@ -10,15 +10,9 @@ function getSiteLocationPath(url){
 	return url.substring(0,window.location.href.lastIndexOf("/")+1);
 }
 
-function getParamValue(paramName){
-	var url = window.location.search.substring(1); //get rid of "?" in querystring
-	var qArray = url.split('&'); //get key-value pairs
-	for (var i = 0; i < qArray.length; i++) 
-	{
-		var pArr = qArray[i].split('='); //split key and value
-		if (pArr[0] == paramName) 
-			return pArr[1]; //return value
-	}
+function getParamValue(paramName) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(paramName) ?? undefined;
 }
 
 async function ensureJsonData(d, callback, errorCallback)
