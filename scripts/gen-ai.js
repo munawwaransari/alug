ef507e60841a4dc4b6f7e4c1d7319d3941986cc1
munawwaransari <munawwar_ali@yahoo.com>
2026-05-04 16:09:44 +0530

    updated: ai

--- scripts/gen-ai.js
+++ scripts/gen-ai.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const GEMINI_MODEL = "gemini-2.5-flash"; 
 
 if(!window.runGeminiGenAISearch)
-window.runGeminiGenAISearch = function (apiKey, aimodel, prompt, useSelLang){
+window.runGeminiGenAISearch = function (apiKey, aimodel, flags, prompt, useSelLang){
 	
     return new Promise(async function (resolve, reject) {
         if(apiKey == undefined || apiKey == ""){
@@ -15,11 +15,22 @@ window.runGeminiGenAISearch = function (apiKey, aimodel, prompt, useSelLang){
             prompt += '\n Provide Final response in language: '+ parent.getLang();
         }
 
+        if(flags["HTML"] == true){
+            prompt += "\n Provide response in HTML format.";
+        }
+
         try {
             const genAI = new GoogleGenerativeAI(apiKey);
             var modelname = aimodel == "default" ? GEMINI_MODEL : aimodel;
+            var modalities = flags["IMAGE"] == true ? ["image"] : undefined; 
             const model = genAI.getGenerativeModel({ model: modelname ?? GEMINI_MODEL});
-            var res = await model.generateContent(prompt);
+            var res = (modalities === undefined) ?
+                await model.generateContent(prompt):
+                await model.generateContent({
+                    generationConfig: {
+                        responseModalities: modalities
+                    }
+                });
             resolve(res);
         } 
         catch (error) {
