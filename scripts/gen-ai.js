22e90356bf56ee0348443682c45b1f4b7735c584
munawwaransari <munawwar_ali@yahoo.com>
2026-09-05 19:45:17 +0530

    updated ai models

--- scripts/gen-ai.js
+++ scripts/gen-ai.js
@@ -1,38 +1,29 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import OpenAI from "openai";
+import OpenAI from "@openai";
 
-const GEMINI_MODEL = "gemini-2.5-flash"; 
-
-if(!window.runGeminiGenAISearch)
-window.runGeminiGenAISearch = function (apiKey, aimodel, flags, prompt, useSelLang){
-	
+if(!window.runAISearch)
+window.runAISearch = function (name, options){
     return new Promise(async function (resolve, reject) {
-        if(apiKey == undefined || apiKey == ""){
-            reject("Error: API KEY is required!");
-            return;
-        }
+        try 
+        {
+            var p = handleAIArgs(options);
+            switch(name){
+                case "google":
+                    resolve(runGeminiAISearch(options));
+                    break;
+                
+                case "openapi":
+                    resolve(runOpenAISearch(options));
+                    break;
 
-        if(useSelLang){
-            prompt += '\n Provide Final response in language: '+ parent.getLang();
-        }
+                case "anthropic":
+                    resolve(runAnthropicAISearch(options));
+                    break;
 
-        if(flags["HTML"] == true){
-            prompt += "\n Provide response in HTML format.";
-        }
-
-        try {
-            const genAI = new GoogleGenerativeAI(apiKey);
-            var modelname = aimodel == "default" ? GEMINI_MODEL : aimodel;
-            var modalities = flags["IMAGE"] == true ? ["image"] : undefined; 
-            const model = genAI.getGenerativeModel({ model: modelname ?? GEMINI_MODEL});
-            var res = (modalities === undefined) ?
-                await model.generateContent(prompt):
-                await model.generateContent({
-                    generationConfig: {
-                        responseModalities: modalities
-                    }
-                });
-            resolve(res);
+                default:
+                    reject("Providei is not integrated yet!");
+                    break;
+            }
         } 
         catch (error) {
             reject("Error: " + error.message);
@@ -40,42 +31,50 @@ window.runGeminiGenAISearch = function (apiKey, aimodel, flags, prompt, useSelLa
     });
 }
 
-const OPENAI_MODEL = "gpt-3.5-turbo";
+//google
+async function runGeminiAISearch(options){
+    const genAI = new GoogleGenerativeAI(options.apiKey);
+    const model = genAI.getGenerativeModel({ model: options.aimodel });
+    return (options.flags["IMAGE"] !== true) ?
+        await model.generateContent(options.prompt):
+        await model.generateContent(options.prompt,{
+            generationConfig: {
+                responseModalities: ["image"]
+            }
+        });
+}
 
-if(!window.runOpenAISearch)
-window.runOpenAISearch = function (apiKey, aimodel, flags, prompt, useSelLang){
-	
-    return new Promise(async function (resolve, reject) {
-        if(apiKey == undefined || apiKey == ""){
-            reject("Error: API KEY is required!");
-            return;
-        }
-
-        if(useSelLang){
-            prompt += '\n Provide Final response in language: '+ parent.getLang();
-        }
-
-        if(flags["HTML"] == true){
-            prompt += "\n Provide response in HTML format.";
-        }
-
-        try {
-            const openai = new OpenAI({ 
-                //dangerouslyAllowBrowser: true,
-                apiKey: apiKey 
-            });
-
-            var modelname = aimodel == "default" ? OPENAI_MODEL : aimodel;
-            const completion = await openai.chat.completions.create({
-                model: modelname,
-                messages: [
-                    {"role": "user", "content": prompt}
-                ]
-            });
-            resolve(chatCompletion.choices[0].message.content);
-        } 
-        catch (error) {
-            reject("Error: " + error.message);
-        }
+//opeai
+async function runOpenAISearch(options){	
+    const openai = new OpenAI({ 
+        dangerouslyAllowBrowser: true,
+        apiKey: options.apiKey
     });
+
+    if(options.flags["IMAGE"] == true){
+        return("TODO: Image call");
+    }
+    const completion = await openai.chat.completions.create({
+        model: options.aimodel,
+        messages: [
+            {"role": "user", "content": options.prompt}
+        ]
+    });
+    return chatCompletion.choices[0].message.content;
+}
+
+function handleAIArgs(options){
+    if(options.apiKey == undefined || options.apiKey == ""){
+        throw new Error("API KEY is required!");
+    }
+
+    var p = options.prompt;
+    if(options.useSelLang){
+        p += '\n Provide Final response in language: '+ parent.getLang();
+    }
+
+    if(options.flags["HTML"] == true){
+        p += "\n Provide response in HTML format.";
+    }
+    return p;
 }
