1335f115e9e1cb3ab354c9cc4dd9c919f40803dd
munawwaransari <munawwar_ali@yahoo.com>
2026-09-18 17:37:37 +0530

    updated patterns view

--- scripts/data-util.js
+++ scripts/data-util.js
@@ -2639,3 +2639,9 @@ function getTajweedTable(){
 		<tbody>
 		</table>`;
 }
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
